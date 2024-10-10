f6e3f5e0af0e649ed90cd0bcbaf753938ad27004
Patrik Malmström <popsork@gmail.com>
2024-10-10 18:26:37 +0200

    move backend code to root/src

--- src/index.js
+++ src/index.js
@@ -6,14 +6,11 @@ dotenv.config()
 
 const app = express()
 app.use(express.json())
+app.use(express.static('public'));
 
-connectDb()
-
-app.get('/', (req, res) => {
-    res.send('Equilibrium API is running')
-})
+connectDb();
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT => () => {
+app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
-})
+});
