39a85d8ba32a1449f1da7dad6ab83488907ffce3
J0113 <keskitalo_6@hotmail.com>
2024-10-11 16:25:58 +0200

    testing pages, homepage done, on to next

---
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Homepage.scss'
+
+const HomePage = () => {
+    return (
+        <div className="homepage">
+            <h1>Welcome to Equilibrium!</h1>
+            <p>Your journey to balance begins here.</p>
+        </div>
+    )
+}
+
+export default Homepage
