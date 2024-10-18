ef880bd71ca4ac554bd32c01cae8d8a8491f7c4d
J0113 <keskitalo_6@hotmail.com>
2024-10-18 23:49:46 +0200

    Create simple Frontend template

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Header.scss'
 
 const Header = () => {
     return (
         <div className='header'>
-            <h1>EQUILIBRIUM</h1>
+            <h1><Link to='/'>EQUILIBRIUM</Link></h1>
         </div>
     )
 }
--- frontend/src/pages/TodoPage/TodoPage.jsx
+++ frontend/src/pages/TodoPage/TodoPage.jsx
@@ -4,8 +4,21 @@ import './TodoPage.scss'
 const TodoPage = () => {
     return (
         <div className='todo-page'>
-            <h1>To do:</h1>
-            <p>Lots of stuff!</p>
+            <form className='todo-form'>
+                <h1>Add a new Todo</h1>
+                <div className='form-group'>
+                    <label htmlFor='todo'>Todo</label>
+                    <input type='text' id='todo' name='todo' placeholder='Enter your todo' required />
+                </div>
+                <div className='form-group'>
+                    <label htmlFor='description'>Description (optional)</label>
+                    <textarea id='description' name='description' placeholder='Add a description'></textarea>
+                </div>
+                <div className='buttons'>
+                    <button type='submit' className='custom-button'>Add Todo</button>
+                    <button className='custom-button'>View todo list</button>
+                </div>
+            </form>
         </div>
     )
 }
