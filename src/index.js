5cde0f6925d6a4139bd109c659cfb51c9dc82e7b
J0113 <keskitalo_6@hotmail.com>
2024-10-19 12:18:58 +0200

    Fix routing for users

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-const express = require('express')
 const dotenv = require('dotenv')
+dotenv.config()
+const express = require('express')
 const connectDb = require('./database/db')
 const path = require('path')
 const userRoutes = require('./routes/userRoutes') 
 
-dotenv.config()
+
 
 const app = express()
 app.use(express.json())
@@ -13,8 +14,7 @@ app.use(express.static('public'));
 connectDb();
 
 
-app.use('/api/account/register', userRoutes)
-
+app.use('/api/account', userRoutes)
 
 
 
@@ -23,7 +23,7 @@ app.get('*', (req,res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`)
+    console.log(`Server running on http://localhost:${PORT}`);
 });
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/userController')
 
-router.post('/api/account/register', userController.registerUser)
+router.post('/register', userController.createUser)
+router.get('/users', userController.getAllUsers)
 
 module.exports = router
