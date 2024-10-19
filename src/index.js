41b425e20d6f5d371c2889972b15821886aae860
J0113 <keskitalo_6@hotmail.com>
2024-10-19 13:57:47 +0200

    Fix configuration for backend

--- src/index.js
+++ src/index.js
@@ -27,3 +27,8 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
+
+
+// npm run dev <-- backend startup
+// npm run frontend:dev
+// git push dokku main
